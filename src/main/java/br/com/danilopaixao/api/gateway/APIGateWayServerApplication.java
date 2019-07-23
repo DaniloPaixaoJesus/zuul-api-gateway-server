8cef1bf8bd4c4f3fa5c25d1879f4d943667ed856
Danilo GingaOne <danilo.paixao@gingaone.com>
2019-07-23 20:42:13 -0300

    set up cross

--- src/main/java/br/com/danilopaixao/api/gateway/APIGateWayServerApplication.java
+++ src/main/java/br/com/danilopaixao/api/gateway/APIGateWayServerApplication.java
@@ -2,9 +2,12 @@ package br.com.danilopaixao.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 //@EnableDiscoveryClient
@@ -15,6 +18,24 @@ public class APIGateWayServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(APIGateWayServerApplication.class, args);
+    }
+    
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 //    
 //    @Bean
